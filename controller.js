3c6cddc134495866ce835ed8c4671fb9c666214a
ZachTheMighty <promoted66@gmail.com>
2026-09-07 11:15:52 +0300

    add: route for displaying sign up form

--- controller.js
+++ controller.js
@@ -1,5 +1,8 @@
 const homeGet = (req, res) => res.render("home.ejs");
 
+const signUpGet = (req, res) => res.render("sign_up.ejs");
+
 module.exports = {
   homeGet,
+  signUpGet,
 };
--- router.js
+++ router.js
@@ -4,5 +4,6 @@ const controller = require("./controller.js");
 const router = Router();
 
 router.get("/", controller.homeGet);
+router.get("/sign-up", controller.signUpGet);
 
 module.exports = router;
