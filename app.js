38d268d5ca3048c3bbb3fb548ca140055f40b378
ZachTheMighty <promoted66@gmail.com>
2026-09-08 06:50:40 +0300

    add: logout route

--- app.js
+++ app.js
@@ -44,6 +44,12 @@ const dashboardRouter = require("./routes/dashboardRouter.js");
 app.use("/sign-up", signUpRouter);
 app.use("/login", logInRouter);
 app.use("/dashboard", dashboardRouter);
+app.use("/logout", (req, res) =>
+  req.logout((error) => {
+    if (error) return error;
+    res.redirect("/");
+  }),
+);
 app.use("/", (req, res) => res.render("home.ejs"));
 
 const port = process.env.NODE_SERVER_PORT;
