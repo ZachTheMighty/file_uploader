b0c577b9933a5bc435b28a040d8ebe81b7b4c27b
ZachTheMighty <promoted66@gmail.com>
2026-09-08 06:48:51 +0300

    add: constraint so only authenticated users have access to dashboard route

--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -3,6 +3,11 @@ const controller = require("../controllers/dashboardController.js");
 
 const router = Router();
 
+router.use((req, res, next) => {
+  if (req.isAuthenticated()) return next();
+  res.status(401).json({ msg: "You need to log in to view this resource." });
+});
+
 router.get("/", controller.dashboardGet);
 
 module.exports = router;
